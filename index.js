7049f263454c3b955e9316e1d8f08f5cd711e398
Abdykapar <abdykaparkamilov@mail.ru>
2017-07-02 01:14:37 +0600

    User connection and disconnection

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ server.listen(3000)
 app.get('/',function(request,response){
     response.sendFile(__dirname+'/index.html');
 });
-
+var count = 0;
 io.on('connection', function(socket){
+    count++;
+    console.log(count+' User connected');
     socket.on('chat.message', function(message){
         io.emit('chat.message', message);
     });
+    socket.on('disconnect',function(){
+        count--;
+        console.log('User was disconnected');
+       io.emit('chat.message','User was disconnected'); 
+    });
+    
 });
